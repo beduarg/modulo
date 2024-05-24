67aeef1682c76ee40a20dd8abd5008ea1ed3628b
beduarg <mbedeschi@jmawireless.com>
2024-05-24 12:56:30 +0200

    modulo eseguibile direttamente

--- aritmetica.py
+++ aritmetica.py
@@ -8,3 +8,21 @@ def mul(a, b):
     return a * b
 def div(a, b):
     return a / b
+
+if __name__ == "__main__":
+    import sys
+    ops = dict(add=add, sub=sub, mul=mul, div=div)
+
+    choice = input("seleziona un'operazione [add/sub/mul/div]: ")
+
+    if choice not in ops:
+        sys.exit('Operazione non valida!')
+        
+    op = ops[choice]
+    try:
+        a = float(input('Inserisci il primo valore: '))
+        b = float(input('Inserisci il secondo valore: '))
+    except ValueError as err:
+        sys.exit('Valore non valido: {}'.format(err))
+
+    print('Il risultato è:', op(a, b))
